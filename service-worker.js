d5062778aea8ce73ce0f3b4c19be405a3f8406fa
chiyu-git <873705939@qq.com>
2019-03-29 15:42:55 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a2a5fd1ff22b27e233e31bb5e5db6e3.js"
+  "/time_is_money_client/precache-manifest.03569abaac4c95f8f2082f9ea97c1b85.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/time_is_money_client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/redux/reducers.js
+++
@@ -1,8 +0,0 @@
-/* 整合所有组件的reducers */
-
-import playInfo from '../components/NBack/Play/PlayRedux'
-
-import { combineReducers } from 'redux';
-
-export default combineReducers({playInfo})
-// {playInfo:{playResultLIST:[],playLevel:0,playTime:0},}
