1635fe56f406615758fb6ffd87b04fd2d6b71cbe
chiyu-git <873705939@qq.com>
2019-03-31 10:26:34 +0800

    add localStorage

---
+++ build/precache-manifest.779fce53369cce6a0824202c6462952f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9860fcb6e1ebb6d873a2bac3634d407",
+    "url": "/time_is_money_client/static/media/ding.b9860fcb.mp3"
+  },
+  {
+    "revision": "5630c2d1bd73efd0f94a9034af5c081f",
+    "url": "/time_is_money_client/static/media/click.5630c2d1.mp3"
+  },
+  {
+    "revision": "34963dd40b7e7a35f9bd16967a68dc05",
+    "url": "/time_is_money_client/static/media/alphabet.34963dd4.mp3"
+  },
+  {
+    "revision": "1d204beee5b63a98bd18",
+    "url": "/time_is_money_client/static/js/runtime~main.1d204bee.js"
+  },
+  {
+    "revision": "f38cae85bd2ba0276d27",
+    "url": "/time_is_money_client/static/js/main.f38cae85.chunk.js"
+  },
+  {
+    "revision": "ec3c351d3640d160becf",
+    "url": "/time_is_money_client/static/js/2.ec3c351d.chunk.js"
+  },
+  {
+    "revision": "36d634765f3e2f1272c393c2aab7bb92",
+    "url": "/time_is_money_client/index.html"
+  }
+];
